8804f9c83a7ae71b84329b3fe2e2ebe4f2b8875c
gmarqueszx <bielpesso@gmail.com>
2026-07-07 13:05:44 -0300

    chore: scaffold do backend Spring Boot (pom.xml + application class)
    Substitui o esqueleto Maven padrao do IntelliJ pela base do projeto
    conectsol-backend (Spring Boot 3.3.13 / Java 21), com dependencias de
    web, JPA, security, flyway, lombok, springdoc e jjwt.

---
+++ src/main/java/br/com/conectsol/backend/ConectsolBackendApplication.java
@@ -0,0 +1,12 @@
+package br.com.conectsol.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConectsolBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConectsolBackendApplication.class, args);
+    }
+}
